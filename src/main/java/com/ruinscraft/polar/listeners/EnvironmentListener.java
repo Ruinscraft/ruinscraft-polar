c819f0a84f41b5a0c71c9b770c3cf31aad72c398
drewdorris <contact@dorrisd.com>
2019-01-01 12:08:02 -0500

    add some more framework

--- src/main/java/com/ruinscraft/polar/listeners/EnvironmentListener.java
+++ src/main/java/com/ruinscraft/polar/listeners/EnvironmentListener.java
@@ -1,9 +1,16 @@
 package com.ruinscraft.polar.listeners;
 
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.world.WorldInitEvent;
+
+import com.ruinscraft.polar.populator.PolarPopulator;
 
 public class EnvironmentListener implements Listener {
 
-	
+	@EventHandler
+	public void onWorldInit(WorldInitEvent event) {
+		event.getWorld().getPopulators().add(new PolarPopulator());
+	}
 
 }
---
+++ src/main/java/com/ruinscraft/polar/populator/PolarPopulator.java
@@ -0,0 +1,16 @@
+package com.ruinscraft.polar.populator;
+
+import java.util.Random;
+
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.generator.BlockPopulator;
+
+public class PolarPopulator extends BlockPopulator {
+
+	@Override
+	public void populate(World world, Random random, Chunk chunk) {
+		// stuff
+	}
+
+}
